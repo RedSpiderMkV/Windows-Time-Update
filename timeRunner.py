1dcb4ccc1c6ed875f1d35e2b8815a4d18d3e5fba
RedSpiderMkV <redspidermkv@hotmail.com>
2015-09-09 23:26:03 +0100

    minor refactor

--- timeRunner.py
+++ timeRunner.py
@@ -11,35 +11,32 @@ def is_connected():
     # see if we can resolve the host name -- tells us if there is
     # a DNS listening
     host = socket.gethostbyname(REMOTE_SERVER)
-    # connect to the host -- tells us if the host is actually
-    # reachable
+    # connect to the host -- tells us if the host is reachable
     s = socket.create_connection((host, 80), 2)
     return True
   except:
-     pass
-  return False
+     return False
 
 def main():
-    
     attempt = 0
     while not is_connected():
-        time.sleep(10)
-        attempt += 1
-        
         # after 10 tries, give up
         if attempt >= 10:
             return;
-    
+
+        time.sleep(10)
+        attempt += 1
+
     timeSyncer = timeSync()
     timeVal = timeSyncer.getTimeWithTimezoneOffset(1)
     dateTime = timeVal.split(' ')
-    
+
     dateParts = dateTime[0].split('-')
     formattedDate = dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0]
-    
+
     dateCommand = 'date ' + formattedDate
     timeCommand = 'time ' + dateTime[1]
-    
+
     os.system(dateCommand)
     os.system(timeCommand)
 
